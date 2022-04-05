b94014c77ae63ae7f4783c865bac8d7cf783d3f7
吴明桓 <hui_zai2007@outlook.com>
2022-04-05 14:54:10 +0800

    Changed Player login events

--- src/main/java/com/hui_cat/event/BossBarEvents.java
+++ src/main/java/com/hui_cat/event/BossBarEvents.java
@@ -1,2 +1,17 @@
-package com.hui_cat.event;public class BossBarEvents {
+package com.hui_cat.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.boss.DragonBattle;
+
+
+public class BossBarEvents implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void initBar (PlayerJoinEvent event){
+
+
+    }
 }
--- src/main/java/com/hui_cat/event/PlayerJoinEvents.java
+++ src/main/java/com/hui_cat/event/PlayerJoinEvents.java
@@ -1,6 +1,9 @@
 package com.hui_cat.event;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,6 +13,7 @@ public class PlayerJoinEvents  implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event){
         String playerID = event.getPlayer().getName();
         event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ENTITY_VILLAGER_TRADE, 100 , 30);
-        event.getPlayer().sendMessage("Welcome to plugin test "+playerID+"!(This message will be removed after release version of the plugin released)");
+        event.getPlayer().sendMessage(ChatColor.DARK_BLUE +"Welcome to hui_cat's plugin test,"+playerID+"!(This message will be removed after the release version of the plugin released)");
+        event.getPlayer().playSound(event.getPlayer().getLocation() , Sound.ENTITY_VILLAGER_AMBIENT , SoundCategory.NEUTRAL , 10 , 100);
     }
 }
