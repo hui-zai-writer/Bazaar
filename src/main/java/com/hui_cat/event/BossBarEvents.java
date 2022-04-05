2ccdbee2ba0afc3022109e68781aea2ddb28af43
吴明桓 <hui_zai2007@outlook.com>
2022-04-05 15:20:44 +0800

    Added loading boss bar.

--- src/main/java/com/hui_cat/event/BossBarEvents.java
+++ src/main/java/com/hui_cat/event/BossBarEvents.java
@@ -1,17 +1,31 @@
 package com.hui_cat.event;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.boss.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.boss.DragonBattle;
+
+import static org.bukkit.Bukkit.getLogger;
 
 
 public class BossBarEvents implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void initBar (PlayerJoinEvent event){
-
-
+        Location loc = event.getPlayer().getLocation();
+        BossBar bar = Bukkit.createBossBar("Verifying your ID..." , BarColor.BLUE , BarStyle.SEGMENTED_20 , BarFlag.PLAY_BOSS_MUSIC);
+        for (double i=0;i<=1;i+=0.05){
+            event.getPlayer().teleport(loc);
+            bar.setProgress(i);
+            try {
+                Thread.sleep(200);
+            }
+            catch (InterruptedException e){
+                getLogger().info("Error! Thread has been interrupted!");
+            }
+        }
     }
 }
--- src/main/java/com/hui_cat/event/PlayerJoinEvents.java
+++ src/main/java/com/hui_cat/event/PlayerJoinEvents.java
@@ -12,7 +12,7 @@ public class PlayerJoinEvents  implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         String playerID = event.getPlayer().getName();
-        event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ENTITY_VILLAGER_TRADE, 100 , 30);
+        event.getPlayer().playSound(event.getPlayer().getLocation() , Sound.ENTITY_VILLAGER_TRADE, 100 , 30);
         event.getPlayer().sendMessage(ChatColor.DARK_BLUE +"Welcome to hui_cat's plugin test,"+playerID+"!(This message will be removed after the release version of the plugin released)");
         event.getPlayer().playSound(event.getPlayer().getLocation() , Sound.ENTITY_VILLAGER_AMBIENT , SoundCategory.NEUTRAL , 10 , 100);
     }
