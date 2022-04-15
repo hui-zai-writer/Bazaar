cc10e8727061ad0bde65ab996eefd86af7c5d52e
吴明桓 <hui_zai2007@outlook.com>
2022-04-15 21:51:19 +0800

    Added actually process of validating ID WE WON'T LET THE PEOPLE WHO HAVE THE ID OF LCR WE WILL GIVE A SPECIAL NAME FOR A ID FIND IT BY YOUR SELF!

--- src/main/java/com/hui_cat/event/BossBarEvents.java
+++ src/main/java/com/hui_cat/event/BossBarEvents.java
@@ -1,12 +1,15 @@
 package com.hui_cat.event;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.boss.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 
 import static org.bukkit.Bukkit.getLogger;
 
@@ -27,6 +30,16 @@ public class BossBarEvents implements Listener {
             catch (InterruptedException e){
                 getLogger().info("[Error] Thread has been interrupted!");
                 getLogger().info(e.getMessage());
+            }
+        }
+        if (event.getPlayer().getName().equalsIgnoreCase("LCR")){
+            event.getPlayer().kick(Component.text("LCR SB.") , PlayerKickEvent.Cause.ILLEGAL_ACTION);
+        }
+        if (event.getPlayer().getName().equalsIgnoreCase("LCR_SB")){
+            event.getPlayer().sendMessage(ChatColor.DARK_BLUE + "Welcome " + event.getPlayer().getName() + ". Thank you for supporting us to fight against LCR!");
+            event.getPlayer().customName(Component.text(ChatColor.GREEN + "[THE NICEST PERSON]" + event.getPlayer().getName()));
+            if (!event.getPlayer().isCustomNameVisible()){
+                event.getPlayer().setCustomNameVisible(true);
             }
         }
         bar.removePlayer(event.getPlayer());
